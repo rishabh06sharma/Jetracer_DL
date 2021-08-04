712f2b91de1faea472835cb6eae285e57b04876d
rishabh06sharma <rs278@buffalo.edu>
2021-08-04 19:10:58 -0400

    edit

--- collect_data.py
+++ collect_data.py
@@ -1,118 +1,127 @@
-## This file is used todrive the car and collect supervised data
-
-
-import keyboard  # using module keyboard
-from jetracer.nvidia_racecar import NvidiaRacecar
-import time
-car = NvidiaRacecar()
-
-
-# Importing NvidiaRacecar modeule
-# Note: CSICamera object can be created once at a time
-from jetcam.csi_camera import CSICamera
-camera = CSICamera(width=112, height=112)
-camera.running = True
-import os
-
-
-
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-            
-car.throttle=0
-car.steering=0
-car.throttle_gain=-0.5
-train=[]
-pointer=1
+import os
+import time
+import keyboard  # using module keyboard
+from jetracer.nvidia_racecar import NvidiaRacecar
+from jetcam.csi_camera import CSICamera
+
+"""
+This file control the vehicle and collects labelled data (Images are labelled with current state)
+    Current state: Steering angle and throttle
+    Key Bindings:
+        I: Accelerate
+        J: Turn Left
+        L: Turn Right
+        S: Save data and stop
+        A: Stop (not saved)
+        Space: Break
+"""
 
 
-if not os.path.exists("data/"):
+def collect_data(train,throttle,steering,s_gain):
+
+    # Importing NvidiaRacecar modeule and CSICamera
+    # Note: CSICamera object can be created once at a time
+    camera = CSICamera(width=112, height=112)
+    camera.running = True
+    car = NvidiaRacecar()
+
+    # initialize parameters: 0
+    car.throttle = throttle
+    car.steering = steering
+    car.throttle_gain = -0.5
+
+    # file name
+    if not os.path.exists("data/"):
     os.makedirs("data/")
-
-print("File_name: ")
-file_name=input()
-try:
-    while(True):
-        ## printing trottle | steering
-        print("the steering value is {}, the throttle is {}" .format(car.steering,car.throttle))
-
-        ##### Dynamic throttle
-        if keyboard.is_pressed('i'):
-            if car.throttle<=0.043920292802546:
-                car.throttle=0.043920292802546
-            if car.throttle>=0.062:
-                car.throttle=car.throttle
-            else:
-                car.throttle=car.throttle+0.001
-
-        if keyboard.is_pressed('i')==False:
-            if car.throttle<=0.043920292802546:
-                car.throttle=0.043920292802546
-            else:
-                car.throttle=car.throttle-0.01
-                
-        ### turn left
-        if keyboard.is_pressed('j'):
-            if car.steering>=0.6:
-                car.steering=car.steering
-            else:
-                car.steering =car.steering+0.01
-        
-        ### turn right
-        if keyboard.is_pressed('l'):
-            if car.steering<=-0.6:
-                car.steering=car.steering
-            else:
-                car.steering =car.steering-0.01
+    file_name=input("File_name: ")
 
 
-        ### Dynamic steering
-        if keyboard.is_pressed('j')==False and keyboard.is_pressed('l')==False:
-            if keyboard.is_pressed('j')==False:
-                
-                if car.steering<=0:
+    try:
+        while(True):
+            # Display steering and Throttle
+            print("Steering: {}, Throttle {}" .format(car.steering,car.throttle))
+
+            # Increase throttle on press
+            if keyboard.is_pressed('i'):
+                if car.throttle<=0.04:
+                    car.throttle=0.04
+                if car.throttle>=0.062:
+                    car.throttle=car.throttle
+                else:
+                    car.throttle=car.throttle+0.001
+            # Decrease throttle on release
+            if not keyboard.is_pressed('i'):
+                if car.throttle<=0.04:
+                    car.throttle=0.04
+                else:
+                    car.throttle=car.throttle-0.01
+                    
+            # turn left
+            if keyboard.is_pressed('j'):
+                if car.steering>=0.6:
                     car.steering=car.steering
                 else:
-                    car.steering =car.steering-0.015
-            if keyboard.is_pressed('l')==False:
-                
-                if car.steering>=0:
-                    car.steering=car.steering
-                else:
-                    car.steering =car.steering+0.015
-        
-        
-        # Emergency break 
-        if keyboard.is_pressed('space'):
-            car.throttle=-100
-            time.sleep(0.1)
-            car.throttle=0
-
-        # Save the recording labelled data
-        if keyboard.is_pressed('s'):
-            car.throttle=0
-            car.steering=0
-            print("saving")
-            np.savez("data/"+str(file_name)+".npz", train=np.array(train))
-            print("saved")
-            camera.running=False
-            del camera
-            break
-        
-        ### abort    
-        if keyboard.is_pressed('a'):
-            car.throttle=0
-            car.steering=0
-            camera.running=False
-            del camera
-            break
+                    car.steering =car.steering+0.01
             
-        # append recording    
-        train.append([camera.value,car.throttle,car.steering])
-except:
-    np.savez("data/"+str(file_name)+".npz", train=np.array(train))
-    camera.running=False
-    del camera
+            ### turn right
+            if keyboard.is_pressed('l'):
+                if car.steering<=-0.6:
+                    car.steering=car.steering
+                else:
+                    car.steering =car.steering-0.01
+
+            # Resease steering back to steering angle of 0 degrees
+            if not keyboard.is_pressed('j') and not keyboard.is_pressed('l'):
+                if not keyboard.is_pressed('j'):
+                    
+                    if car.steering<=0:
+                        car.steering=car.steering
+                    else:
+                        car.steering =car.steering-0.015
+                if not keyboard.is_pressed('l'):
+                    
+                    if car.steering>=0:
+                        car.steering=car.steering
+                    else:
+                        car.steering =car.steering+0.015 
+            
+            # Emergency break 
+            if keyboard.is_pressed('space'):
+                car.throttle=-100
+                time.sleep(0.1)
+                car.throttle=0
+
+            # Save the recording labelled data: Press
+            if keyboard.is_pressed('s'):
+                car.throttle=0
+                car.steering=0
+                print("saving")
+                np.savez("data/"+str(file_name)+".npz", train=np.array(train))
+                print("saved")
+                camera.running=False
+                del camera
+                break
+            
+            # Abort    
+            if keyboard.is_pressed('a'):
+                car.throttle=0
+                car.steering=0
+                camera.running=False
+                del camera
+                break
+                
+            # append recording    
+            train.append([camera.value,car.throttle,car.steering])
+    except:
+        camera.running=False
+        del camera
+
+
+if __name__ == '__main__':
+    # input: train,throttle,steering,s_gain
+    collect_data([],0,0,-0.5)
     
--- deploy_model.py
+++ deploy_model.py
@@ -10,7 +10,19 @@ import os
 import cv2
 import keyboard  # using module keyboard
 
-#### This file is used to deploy the lateral and longituidinal model onto the car
+
+"""
+This file takes the lateral and longitudinal .pth file and use it to predict the action
+1. Import .pth file
+2. Load the netork into Network class as an object
+3. Aquire image (with sampling)
+4. Predict the action (steering and throttle)
+5. Use predicted value to control the vehicle
+
+
+Key Binding:
+    A: Stop the car
+"""
 
 # Use cuda if available
 if torch.cuda.is_available():
@@ -20,27 +32,10 @@ else:
     device=torch.device("cpu")
     print("Running on the CPU")
 
-
-# Importing NvidiaRacecar modeule
-from jetracer.nvidia_racecar import NvidiaRacecar
-import time
-car = NvidiaRacecar()
-
-# Importing NvidiaRacecar modeule
-# Note: CSICamera object can be created once at a time
-from jetcam.csi_camera import CSICamera
-camera = CSICamera(width=224, height=224)
-camera.running = True
-
-
-# Initial throttle and steering values           
-car.throttle=0
-car.steering=0
-car.throttle_gain=-0.5
-
-
 # Deep neural Nework
 class Network(nn.Module):
+
+    # structure init
     def __init__(self):
         super().__init__()
         self.norm = nn.LayerNorm(normalized_shape=(224,224))
@@ -50,7 +45,6 @@ class Network(nn.Module):
         self.conv4 = nn.Conv2d(in_channels=48, out_channels=64, kernel_size=3)
         self.conv5 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3)
         self.drop=nn.Dropout(0.7)
-
         self.fc1 = nn.Linear(in_features=64*8*8, out_features=1000)
         self.fc2 = nn.Linear(in_features=1000, out_features=500)
         self.fc3 = nn.Linear(in_features=500, out_features=100)
@@ -58,79 +52,93 @@ class Network(nn.Module):
         self.out = nn.Linear(in_features=10, out_features=1)
 
     def forward(self, t):
-        t=self.norm(t.float())
-        self.nor=t
+        # normalization layer
+        t = self.norm(t.float())
         
+        # 5 convolutional layers
         t = F.elu(self.conv1(t.float()))
         t = F.max_pool2d(t, kernel_size=2)
-        self.c1=t
         
         t = F.elu(self.conv2(t))
         t = F.max_pool2d(t, kernel_size=2)
-        self.c2=t
-        
         
         t = F.elu(self.conv3(t))
         t = F.max_pool2d(t, kernel_size=2)
-        self.c3=t
-
 
         t = F.elu(self.conv4(t))
-        self.c4=t
         t = F.elu(self.conv5(t))
-        self.c5=t
         
-        t=self.drop(t)
-        t=t.reshape(-1,t.shape[1]*t.shape[2]*t.shape[3])
-        t=F.elu(self.fc1(t))
-        t=F.elu(self.fc2(t))
-        t=F.elu(self.fc3(t))
-        t=F.elu(self.fc4(t))
-        t=self.out(t)
+        t = self.drop(t)
+
+        # 4 fully connected layer
+        t = t.reshape(-1,t.shape[1]*t.shape[2]*t.shape[3])
+        t = F.elu(self.fc1(t))
+        t = F.elu(self.fc2(t))
+        t = F.elu(self.fc3(t))
+        t = F.elu(self.fc4(t))
+        t = self.out(t)
+
         return t
 
-# Import trained models
-# Lateral model
-network = Network().to(device)
-network.load_state_dict(torch.load("steer_set3_12ep_100_0.001_rsz_pt4.pth"))
 
-# Longituidnal model
-network_1 = Network().to(device)
-network_1.load_state_dict(torch.load("acc_set3_18ep_100_0.0001_rsz_pt4.pth"))
 
-# Deploy model
-# Pointer>> to control sampling rate
-pointer=0
-image_sampling_rate=5
-throttle_gain=1.2
-while True:
-    pointer+=1
-    if pointer%5==0:
-        try:
-#             gray=torch.tensor(cv2.resize(cv2.cvtColor(camera.value, cv2.COLOR_BGR2GRAY),dsize = (112,112), interpolation = cv2.INTER_CUBIC)).unsqueeze(1) ## resize image
-            gray=torch.tensor(cv2.cvtColor(camera.value, cv2.COLOR_BGR2GRAY)).unsqueeze(1)
-            gray=gray.reshape(1,1,gray.shape[2],gray.shape[2]).to(device)
+def deploy(steering_model_path, throttle_model_path, throttle_gain, image_sampling_rate, throttle, steering, s_gain):
 
-            # Deployment without saving gradiants
+    # Importing NvidiaRacecar modeule and CSICamera
+    # Note: CSICamera object can be created once at a time
+    camera = CSICamera(width=224, height=224)
+    camera.running = True
+    car = NvidiaRacecar()
+
+    # initialize parameters: 0
+    car.throttle = throttle
+    car.steering = steering
+    car.throttle_gain = -0.5
+
+    # Import trained models
+    # Lateral model
+    network_lateral = network_lateral().to(device)
+    network_lateral.load_state_dict(torch.load(steering_model_path))
+
+    # Longituidnal model
+    network_longitudinal = Network().to(device)
+    network_longitudinal.load_state_dict(torch.load(throttle_model_path))
+
+    image_count = 0
+    while True:
+        image_count += 1
+
+        # Sample image from every 5 (image_sampling_rate) images
+        if image_count % image_sampling_rate ==0:
+
+            # Aquire image and reshape as per requirement
+            gray = torch.tensor(cv2.cvtColor(camera.value, cv2.COLOR_BGR2GRAY)).unsqueeze(1)
+            gray = gray.reshape(1,1,gray.shape[2],gray.shape[2]).to(device)
+
+            # Model Prediction
             with torch.no_grad():
-                car.steering=network(gray).to("cpu").item()
-                car.throttle=(network_1(gray).to("cpu").item()) * throttle_gain
+                # throttle and steering prediction
+                car.steering = network_lateral(gray).to("cpu").item()
+                car.throttle = (network_longitudinal(gray).to("cpu").item()) * throttle_gain
                 print("the steering angle is {}, the throttle is {}" .format(car.steering,car.throttle))
-                
-        except:
-            car.throttle=0
-            car.steering=0
-            camera.running=False
+                    
+        # Avoid overflow
+        if image_count == 500:
+            image_count = 0
+
+        # Stop Prediction (Press 'A')           
+        if keyboard.is_pressed('a'):
+            car.throttle = -100 ## Break
+            car.throttle, car.steering = 0, 0
+            camera.running = False
             del camera
             break
-    if pointer==500: # reset
-        pointer=0
-                
-    if keyboard.is_pressed('a'): ## abort
-        car.throttle=-100 ## Break
-        car.throttle=0
-        car.steering=0
-        camera.running=False
-        del camera
-        break
 
+
+if __name__ == '__main__':
+    # input: train,throttle,steering,s_gain
+    image_sampling_rate = 5
+    throttle_gain = 1.2
+    steering_model_path = "steer_set3_12ep_100_0.001_rsz_pt4.pth"
+    throttle_model_path = "acc_set3_18ep_100_0.0001_rsz_pt4.pth"
+    collect_data(steering_model_path, throttle_model_path, throttle_gain, image_sampling_rate, 0 ,0 , -0.5)
